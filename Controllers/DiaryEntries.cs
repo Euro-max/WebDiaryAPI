5a7327950e3295feacd813341bbbf5fbe9deb743
Ahmed Wael <83090095+Euro-max@users.noreply.github.com>
2024-08-01 03:52:56 -0700

    Add files via upload

---
+++ Controllers/DiaryEntries.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiaryEntries : ControllerBase
+    {
+
+            // GET: api/<WebAPI>
+
+            public class DiaryEntriesController : ControllerBase
+            {
+                private readonly ApplicationDbContext _context;
+                public DiaryEntriesController(ApplicationDbContext context)
+                {
+                    _context = context;
+                }
+                [HttpGet]
+
+                public IEnumerable<AP> GetDiaryEntries()
+                {
+                    return _context.DiaryEntries.ToList();
+                }
+
+
+            }
+        }
+    }
+
+
+
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+namespace WebAPI.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<AP> DiaryEntries { get; set; }
+    }
+}
